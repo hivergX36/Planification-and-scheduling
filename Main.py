0f0ab0af3448c184369fbdd942d25f1d4c1dfe52
hivergX36 <vic.herve44@gmail.com>
2024-06-20 02:38:42 +0200

    update_20_06

--- Main.py
+++ Main.py
@@ -2,9 +2,7 @@ from class_schedule import *
 import re 
 
 Problem = schedule_Lmax()
+Problem.parse_data("data.text")
+Problem.solve_Lmax()
 
-datProb = Problem.parse_data("data.text")
 
-datProb = [datProb[i] for i in range(len(datProb)) if datProb[i] != "\n"]
-datProb = [ re.sub("\n", "", datProb[i]) for i in range(len(datProb))]
-print(datProb)
--- class_schedule.py
+++ class_schedule.py
@@ -1,15 +1,46 @@
+import re 
 class schedule_Lmax:
     
     def __init__(self) -> None:
         self.pj = []
         self.dj = []
-        self.prec = []
+        self.solution = {}
+        
+        
+    def checknumber(self,lignes,indice):
+        ParsedList = []
+        compteur1 = 0
+        compteur2 = 0
+        while(lignes[indice][compteur1] != "," and lignes[indice][compteur2] != ","):
+              while(lignes[indice][compteur2] != ","):
+                    compteur2 += 1
+              print(lignes[indice][compteur1:compteur2])
+              ParsedList.append(int(lignes[indice][compteur1:compteur2]))
+              compteur1 = compteur2 + 1
+              compteur2 = compteur1
+
+   
+              if compteur1 > len(lignes[indice]) - 1:
+                    break
+        return ParsedList
+        
+    
         
     def parse_data(self,text):
         fichier = open(text, "r",encoding="utf8")
         lines = fichier.readlines()
-        return lines 
+        lines = [lines[i] for i in range(len(lines)) if lines[i] != "\n"]
+        lines = [ re.sub("\n", "", lines[i]) for i in range(len(lines))]
+        print(lines)
+        self.pj = self.checknumber(lines,1)
+        self.dj = self.checknumber(lines,3)
+        print("dj", self.dj)
+        print("pj", self.pj)
         
+    def solve_Lmax(self):
+        self.solution = {str(i): self.dj[i] for i in range(len(self.dj))}
+        sorted(self.solution.items(), key=lambda k:k[1])
+        print(self.solution)
     
         
         
